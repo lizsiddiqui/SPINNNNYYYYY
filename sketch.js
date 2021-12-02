4fb08922f5072cfbb156a8894a5e19f5f24e254f
Cookie <90225887+lizsiddiqui@users.noreply.github.com>
2021-12-02 18:59:05 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,72 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var btn2;
+
+var angle=60;
+
+
+function setup() {
+  createCanvas(400,400);
+
+  engine = Engine.create();
+  world = engine.world;
+  
+   var ball_options = {
+    restitution: 0.95,
+    frictionAir:0.01
+  }
+   
+   var ground_options ={
+     isStatic: true
+   };
+  
+   btn2 = createImg('up.png');
+  btn2.position(350,30);
+  btn2.size(50,50);
+  btn2.mouseClicked(vForce);
+
+  spinner1 = new Ground(100,300,100,20);
+  spinner2 = new Ground(300, 350, 100, 20);
+  spinner3 = new Ground(110, 40, 100,20);
+  spinner4 = new Ground(200, 10, 100, 20);
+
+  ball = Bodies.circle(100,10,20,ball_options);
+  World.add(world,ball);
+  
+ 
+  ground = Bodies.rectangle(100,400,650,20,ground_options);
+  World.add(world,ground); 
+  
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+
+function draw() 
+{
+  background(51);
+  Engine.update(engine);
+  
+  spinner1.display();
+  spinner2.display();
+  spinner3.display();
+  spinner4.display();
+
+  ellipse(ball.position.x,ball.position.y,20);
+  rect(ground.position.x,ground.position.y,650,20);
+ 
+//console.log(ground.position.y);
+
+  
+  
+}
+
+function vForce()
+{
+  Matter.Body.applyForce(ball,{x:0,y:0},{x:0,y:-0.05});
+}
+  
